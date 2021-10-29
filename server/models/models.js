0116867e71540aaf136131c8607757720ab4ed76
Yash Diniz <yashdiniz@github.com>
2021-10-29 23:35:21 +0530

    Major update to models.js 1. Added assert function. 2. Changed Nephrology to Pulmonology. 3. Changed specialization to speciality. 4. Changed uuid to phoneNumber (for doctor and patient) 5. Added appointmentSchema. 6. Linked them to the database.

--- server/models/models.js
+++ server/models/models.js
@@ -1,11 +1,20 @@
 const path = require('path'),
     crypto = require('crypto'),
-    rxdb = require('rxdb'),
+    RxDB = require('rxdb'),
     leveldown = require('leveldown');
 
 RxDB.addRxPlugin(require('pouchdb-adapter-leveldb'));
 RxDB.addRxPlugin(require('pouchdb-quick-search'));
 
+/**
+ * Asserts a condition, and throws a message on violation.
+ * @param {boolean} condition Condition to be asserted.
+ * @param {string} message Message to throw on condition violation.
+ */
+ const assert = (condition, message) => {
+    if(!condition) throw message || 'Assertion failed!';
+    else return true;
+};
 
 /**
  * Generates a random string of characters.
@@ -18,25 +27,25 @@ RxDB.addRxPlugin(require('pouchdb-quick-search'));
 
 const doctorSchema = {
     name: 'doctor',
-    title: 'Doctor schema',
+    title: 'Doctor entity schema',
     version: 0,
     description: 'Collection contains doctor details.',
     type: 'object',
     properties: {
-        uuid: {
+        phoneNumber: {
             type: 'string',
             primary: true,
         },
         name: {
             type: 'string',
         },
-        specialization: {
+        speciality: {
             type: 'string',
             enum: [
-                'General Medicine',
+                'GeneralMedicine',
                 'Cardiology',
                 'Opthalmology',
-                'Nephrology',
+                'Pulmonology',
                 'Gynaecology',
                 'Gastroenterology',
                 'Dermatology',
@@ -45,8 +54,8 @@ const doctorSchema = {
             ]
         },
     },
-    required: ['name', 'specialization'],
-    indexes: ['specialization'],
+    required: ['name', 'speciality'],
+    indexes: ['speciality'],
 };
 
 const patientSchema = {
@@ -56,7 +65,7 @@ const patientSchema = {
     description: 'Collection contains patient details.',
     type: 'object',
     properties: {
-        uuid: {
+        phoneNumber: {
             type: 'string',
             primary:true,
         },
@@ -69,7 +78,7 @@ const patientSchema = {
 
 const medicationSchema = {
     name: 'medication',
-    title: 'Medication Schema',
+    title: 'Medication entity Schema',
     version: 0,
     description: 'Collection contains medication and prescription details.',
     type: 'object',
@@ -121,19 +130,14 @@ const medicationSchema = {
 
 const bedSchema = {
     name: 'bed',
-    title: 'Bed Schema',
+    title: 'Bed entity Schema',
     version: 0,
-    description: 'Collection contains bed and prescription details.',
+    description: 'Collection contains bed details.',
     type: 'object',
     properties: {
         uuid: {
             type: 'string',
             primary: true,
-        },
-        patient: {
-            type: 'string',
-            ref: 'patient',
-            final: true,
         },
         bedType: {
             type: 'string',
@@ -148,14 +152,70 @@ const bedSchema = {
         },
         fromDate: {
             type: 'number',
-            description: 'Timestamp to start the medication.',
+            description: 'Timestamp for the patient to start using the bed.',
         },
         toDate: {
             type: 'number',
-            description: 'Timestamp to end the medication.',
+            description: 'Timestamp for the patient to end using the bed.',
+        },
+        patient: {
+            type: 'string',
+            ref: 'patient',
+            unique: true,   // TODO: enforce the unique constraint at service level!
         },
     },
-    required: ['patient', 'bedType', 'fromDate', 'toDate'],
+    required: ['bedType', 'fromDate', 'toDate'],
     indexes: ['patient', 'bedType', 'fromDate', 'toDate'],
 };
 
+const appointmentSchema = {
+    name: 'appointment',
+    title: 'Appointment relationship schema',
+    version: 0,
+    description: 'Collection handles a doctor-patient relationship.',
+    type: 'object',
+    properties: {
+        uuid: {
+            type: 'string',
+            primary: true,
+        },
+        doctor: {
+            type: 'string',
+            ref: 'doctor',
+            final: true,
+        },
+        patient: {
+            type: 'string',
+            ref: 'patient',
+            final: true,
+        },
+        timeslot: {
+            type: 'number',
+        },
+        progressHistory: {
+            type: 'string',
+            default: '',
+            description: 'Doctors use this field to discuss the interaction and progress of a patient\'s appointment',
+        }
+    }
+};
+
+const db = RxDB.createRxDatabase({
+    name: path.join('db/'),
+    adapter: leveldown,
+    eventReduce: false,
+}).catch(console.error);
+
+const collections = db.then(database=> database.addCollections({
+    doctor: {schema: doctorSchema},
+    patient: {schema: patientSchema},
+    medication: {schema: medicationSchema},
+    bed: {schema: bedSchema},
+    appointment: {schema: appointmentSchema},
+})).catch(console.error);
+
+
+module.exports = {
+    db, collections, 
+    assert, generateUUID,
+}
