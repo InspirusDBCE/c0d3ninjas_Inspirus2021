953e848460ac2ef5b41bb9838d97dfe654122134
Yash Diniz <yashdiniz@github.com>
2021-10-30 02:05:04 +0530

    Undoing big problems on models.js

--- client/client/app/Functions/Utils.js
+++ client/client/app/Functions/Utils.js
@@ -1,14 +1,14 @@
 import { doctorName } from "../DummyData/doctorNames"
-import { collections, db } from "./models"
+// import { collections, db } from "./models"
 
 export async function sendSpeciality({navigation, specialityName}){
     // AXIOS TBA?
     // Currently prefering RxDB, experimenting with a tradeoff on speed with security.
-    // const list = doctorName
+    const list = doctorName
     
-    const list = await collections.doctor.find()
-    .where('speciality').eq(specialityName)
-    .exec().toJSON();
+    // const list = await collections.doctor.find()
+    // .where('speciality').eq(specialityName)
+    // .exec().toJSON();
 
     navigation.navigate("SelectDoctorScreen", {list})
 }
