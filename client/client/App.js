399299f55638ecd0a096585d21c7ecfb0b1de494
eeshankeni <eeshan.keni@gmail.com>
2021-10-29 19:00:09 +0530

    added stack navigation
    stack nav

--- client/client/App.js
+++ client/client/App.js
@@ -3,14 +3,37 @@ import { StyleSheet, Text, View } from 'react-native';
 import UserHomePageScreen from './app/Screens/userHomePageScreen';
 import Screen from './app/Components/Screen';
 import AppText from './app/config/AppText';
+import {NavigationContainer} from '@react-navigation/native'
+import {createStackNavigator} from '@react-navigation/stack'
 
 
+const Stack = createStackNavigator()
+const StackNavigator = () => (
+
+    <Stack.Navigator
+      screenOptions={{
+      headerShown: false
+    }}>
+
+        <Stack.Screen name="HomeScreen" component={UserHomePageScreen}/>
+
+
+    </Stack.Navigator>
+
+
+
+)
 
 
 export default function App() {
   return (
       <Screen>
-          <UserHomePageScreen/>
+        <NavigationContainer>
+          
+            <StackNavigator/>
+
+        </NavigationContainer>
+
       </Screen>
 
   );
