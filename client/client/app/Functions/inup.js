89e52d475e12ad06ec64b95bf93ad292d44aedee
Yash Diniz <yashdiniz@github.com>
2021-10-30 11:41:18 +0530

    Reverted due to regression issue.

--- client/client/app/Functions/inup.js
+++ client/client/app/Functions/inup.js
@@ -1,9 +1,27 @@
-import { collections } from "./models";
+// import { collections } from "./models";
 
-async function run() {
-    const doc = await collections.doctor.insert({
-        
-    });
-}
+// export default async function run() {
+//     const doc = await collections.doctor.bulkInsert([{
+//         phoneNumber: '1234567890',
+//         name: 'Yash',
+//         speciality: 'General Medicine'
+//     },
+//     {
+//         phoneNumber: '1023456789',
+//         name: 'Akash',
+//         speciality: 'Cardiology'
+//     },
+//     {
+//         phoneNumber: '9012345678',
+//         name: 'Eeshan',
+//         speciality: 'Pulmonology'
+//     },
+//     {
+//         phoneNumber: '8123456790',
+//         name: 'Leeroy',
+//         speciality: 'Cardiology'
+//     },
+// ]);
+// }
 
-run();
+// run();
